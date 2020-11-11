a3caeabf7fe12e3f7b6aa4544b2f0ecf99e49f2c
Andrew Kane <andrew@chartkick.com>
2020-11-11 02:06:07 -0800

    Fixed commands

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ if (process.platform == 'darwin') {
   run(`wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -`);
   // TODO use elasticsearchVersion
   run(`echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list`);
-  run(`sudo systemctl start elasticsearch`);
   run(`sudo apt-get update`);
   run(`sudo apt-get install elasticsearch`);
+  run(`sudo systemctl start elasticsearch`);
 }
 
 run(`for i in \`seq 1 30\`; do curl -s localhost:9200 && break; sleep 1; done`);
