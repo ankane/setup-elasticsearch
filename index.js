6815c96cf569c5b7e3dc9d0c28dc0640fccec6de
Andrew Kane <andrew@chartkick.com>
2020-11-11 02:05:12 -0800

    Updated Linux

--- index.js
+++ index.js
@@ -19,9 +19,12 @@ if (process.platform == 'darwin') {
   const bin = `/usr/local/opt/elasticsearch@${elasticsearchVersion}/bin`;
   run(`${bin}/elasticsearch -d`);
 } else {
-  if (elasticsearchVersion != 7) {
-  }
+  run(`wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -`);
+  // TODO use elasticsearchVersion
+  run(`echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list`);
   run(`sudo systemctl start elasticsearch`);
+  run(`sudo apt-get update`);
+  run(`sudo apt-get install elasticsearch`);
 }
 
 run(`for i in \`seq 1 30\`; do curl -s localhost:9200 && break; sleep 1; done`);
