d2a3f775b73defeda932f4f4313c739f78cefc45
Andrew Kane <andrew@ankane.org>
2021-12-13 00:07:15 -0800

    Added log4j fix for Elasticsearch < 6.5

--- index.js
+++ index.js
@@ -122,6 +122,15 @@ function fixLog4j() {
   const jvmOptionsPath = path.join(esHome, 'config', 'jvm.options');
   if (!fs.readFileSync(jvmOptionsPath).includes('log4j2.formatMsgNoLookups')) {
     fs.appendFileSync(jvmOptionsPath, '\n-Dlog4j2.formatMsgNoLookups=true\n');
+
+    // needed for Elasticsearch < 6.5
+    // but remove for all versions
+    if (!isWindows()) {
+      const coreJarPath = fs.readdirSync(path.join(esHome, 'lib')).filter(fn => fn.includes('log4j-core-'))[0];
+      run('zip', '-q', '-d', path.join(esHome, 'lib', coreJarPath), 'org/apache/logging/log4j/core/lookup/JndiLookup.class');
+    } else if (elasticsearchVersion < '6.5') {
+      throw 'Elasticsearch version not available';
+    }
   }
 }
 
