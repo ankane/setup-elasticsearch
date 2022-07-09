0fb2778f7a2c20f42dca7b91e77df86da9622c29
Andrew Kane <andrew@ankane.org>
2022-07-09 13:47:05 -0400

    Updated ES versions [skip ci]

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const path = require('path');
 const process = require('process');
 
 const versionMap = {
-  '8': '8.3.0',
+  '8': '8.3.2',
   '7': '7.17.4',
-  '8.3': '8.3.0',
+  '8.3': '8.3.2',
   '8.2': '8.2.3',
   '8.1': '8.1.3',
   '8.0': '8.0.1',
