15b6aa013b15ae4a89c47bcbe7a9c4be0a21009d
Andrew Kane <andrew@ankane.org>
2025-04-01 19:55:42 -0700

    Fixed CI

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ const esHome = path.join(cacheDir, elasticsearchVersion);
 // https://www.elastic.co/support/matrix
 const majorVersion = parseInt(elasticsearchVersion.split('.')[0]);
 const javaHome = majorVersion == 7 ? process.env.JAVA_HOME_11_X64 : process.env.JAVA_HOME_17_X64;
-if (javaHome) {
+if (javaHome && !isWindows()) {
   if (majorVersion == 7) {
     process.env.JAVA_HOME = javaHome;
     addToEnv(`JAVA_HOME=${javaHome}`);
